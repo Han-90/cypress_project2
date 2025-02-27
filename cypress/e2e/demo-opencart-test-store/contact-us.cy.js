e7edd003e81497e3d2437191c7a74767057020f3
Gökhan <goekhan.oezdemir@allianz.de>
2025-02-27 17:27:04 +0100

    test: add an xpath source to contact us test

--- cypress/e2e/demo-opencart-test-store/contact-us.cy.js
+++ cypress/e2e/demo-opencart-test-store/contact-us.cy.js
@@ -1,4 +1,7 @@
 /// <reference types ="Cypress"/>
+/// <reference types ="cypress-xpath"/>
+
+
 
 
 describe("Test contact us form via Demo Opencart Test Store", ()=> {
@@ -6,7 +9,8 @@ describe("Test contact us form via Demo Opencart Test Store", ()=> {
     it("Should be able to submit a succesful submission via contact us form", ( ) => {
 
     cy.visit('https://demo.opencart.com/')
-    cy.get(':nth-child(2) > .list-unstyled > :nth-child(1) > a').click()    
+    //cy.get(':nth-child(2) > .list-unstyled > :nth-child(1) > a').click()   
+    cy.xpath("//div[@class='row']//a[contains(@href,'contact')]").click()
     cy.get('#input-name').type("Chuck Norris")
     cy.get('#input-email').type("chuck.norris@walkertexasranger.de")
     cy.get('#input-enquiry').type("Wenn Chuck Norris in das Wasser fällt wird er nicht nass, das Wasser wird Chuck Norris")
