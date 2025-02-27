6783bf1e8701f35ba56aeafe999c5db72b4b78b4
Gökhan <goekhan.oezdemir@allianz.de>
2025-02-27 16:34:53 +0100

    test: add contact us test

---
+++ cypress/e2e/demo-opencart-test-store/contact-us.cy.js
@@ -0,0 +1,20 @@
+/// <reference types ="Cypress"/>
+
+
+describe("Test contact us form via Demo Opencart Test Store", ()=> {
+    
+    it("Should be able to submit a succesful submission via contact us form", ( ) => {
+
+    cy.visit('https://demo.opencart.com/')
+    cy.get(':nth-child(2) > .list-unstyled > :nth-child(1) > a').click()    
+    cy.get('#input-name').type("Chuck Norris")
+    cy.get('#input-email').type("chuck.norris@walkertexasranger.de")
+    cy.get('#input-enquiry').type("Wenn Chuck Norris in das Wasser fällt wird er nicht nass, das Wasser wird Chuck Norris")
+    cy.get('.text-end > .btn').click()
+
+    
+    })
+
+  
+})
+
